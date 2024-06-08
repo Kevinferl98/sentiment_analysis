527e42e3f85f97f971a7cb448eacb4bbe7114648
Kevin Ferlauto <kevinferl1998@gmail.com>
2024-06-08 16:52:32 +0200

    added saving of text and sentiment analysis to MongoDB

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,25 @@
 from flask import Blueprint, render_template, request, jsonify
 from textblob import TextBlob
+from .mongo import MongoDB
 
 bp = Blueprint('routes', __name__)
+mongo = MongoDB('mongodb://localhost:27017', 'sentimentDB', 'sentiment_analysis')
 
 @bp.route('/')
 def index():
     return render_template('index.html')
 
 @bp.route('/sentiment_analysis', methods=['POST'])
-def sentimenti_analysis():
+def sentiment_analysis():
     request_body = request.json
     if 'text' not in request_body:
         return jsonify({'error': 'No text was passed'}), 400
-    return analyze_sentiment(request_body['text']), 200
+    try:
+        response = analyze_sentiment(request_body['text'])
+        mongo.collection.insert_one(response.copy())
+        return response, 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
     
 
 def analyze_sentiment(text):
